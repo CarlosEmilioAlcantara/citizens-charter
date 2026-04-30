60c8438e2c5efc98647ab269288b8ef93c954b75
CarlosEmilioAlcantara <agq.xyz@gmail.com>
2026-04-30 09:52:01 +0800

    Login page and footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,41 @@
+export default function Footer() {
+  return (
+    <div 
+      className="
+        fixed
+        bottom-0 
+        flex 
+        justify-between 
+        items-center
+        w-full
+        p-1
+        px-2
+        bg-confirm-hover
+        text-background
+        md:p-2
+        md:px-12
+      ">
+      <img className="w-8 md:w-12" src="/spc-logo.png"/>
+
+      <div 
+        className="
+          flex 
+          flex-col 
+          text-xs 
+          items-center 
+          font-bold 
+          tracking-wider 
+          md:flex-row
+          md:text-lg
+          md:gap-1
+          lg:text-xl
+        ">
+        <p>Citizen's Charter System</p>
+        <span className="hidden md:block">-</span> 
+        <p>Lungsod ng San Pablo</p>
+      </div>
+
+      <img className="w-8 md:w-12" src="/bagong-pilipinas.png"/>
+    </div>
+  );
+}
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext } from "react";
 import AuthContext from "../context/AuthContext";
+import Footer from "../components/Footer";
 
 export default function Login() {
   const [name, setName] = useState("");
@@ -40,38 +41,80 @@ export default function Login() {
         className="
           flex 
           flex-col 
-          bg-background 
-          w-[30%] 
-          h-[40%] 
+          items-center
+          gap-9
+          w-10/12
+          p-4
           rounded-sm 
+          bg-background 
+          shadow-2xl/95
           z-10
+          md:w-2/3
+          md:h-[45%]
+          md:p-8
+          lg:w-2/5 
         ">
-        <div>
-          <p className="font-bold text-accent">Login</p>
+        <div className="flex flex-col items-start w-full gap-5">
+          <p className="text-2xl font-bold text-accent md:text-4xl">Login</p>
 
-          <div>
-            <label>Username</label>
+          <div 
+            className="
+              flex 
+              flex-col 
+              w-full
+              p-2
+              border 
+              border-foreground 
+              rounded-sm
+            ">
+            <label className="text-[8px] md:text-[10px]">Username</label>
             <input
               type="text"
               placeholder="Username..."
               value={name}
               onChange={(e) => setName(e.target.value)}
+              className="text-md focus:outline-none md:text-xl"
             />
           </div>
 
-          <div>
-            <label>Password</label>
+          <div 
+            className="
+              flex 
+              flex-col 
+              w-full
+              p-2
+              border 
+              border-foreground 
+              rounded-sm
+            ">
+            <label className="text-[8px] md:text-[10px]">Password</label>
             <input
               type="password"
               placeholder="Password..."
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              className="text-md focus:outline-none md:text-xl"
             />
           </div>
         </div>
 
-        <button onClick={handleLogin}>Login</button>
+        <button 
+          onClick={handleLogin}
+          className="
+            w-1/3
+            p-2
+            rounded-sm
+            bg-accent 
+            text-md
+            hover:bg-confirm-hover
+            text-background 
+            md:w-1/4
+            md:text-xl
+          ">
+            Pumasok
+          </button>
       </div>
+      <Footer/>
     </div>
   );
 }
