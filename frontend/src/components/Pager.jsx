0c473e6f49be742ce48b37ab33a8bb008f0300e8
CarlosEmilioAlcantara <agq.xyz@gmail.com>
2026-05-14 16:45:28 +0800

    Pager enable/disable depending whether on first or last page

--- frontend/src/components/Pager.jsx
+++ frontend/src/components/Pager.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { FiChevronsLeft, FiChevronsRight } from "react-icons/fi";
 
 export default function Pager({ count, next, prev, fetchItems, route }) {
   const [pages, setPages] = useState([]);
+  const currentPage = useRef(1);
   const number = Math.ceil(count / 10);
 
   useEffect(() => {
@@ -18,31 +19,42 @@ export default function Pager({ count, next, prev, fetchItems, route }) {
       overflow-hidden 
     ">
       <span 
-        onClick={() => fetchItems(`${route}?page=1`)}
-        className="
-          bg-accent 
-          text-background 
+        onClick={() => {
+          fetchItems(`${route}?page=1`);
+          currentPage.current = 1;
+        }}
+        className={`
+          ${currentPage.current === 1 ? 
+            'bg-unfocused text-foreground pointer-events-none' : 
+            'bg-accent text-background'
+          }
           py-px
           leading-none
           cursor-pointer
           transition-all
           duration-300
           hover:bg-confirm-hover
-      ">
+      `}>
         <FiChevronsLeft size={34} />
       </span>
       <span 
-        onClick={() => fetchItems(prev)}
-        className="
-          bg-accent 
-          text-background 
+        onClick={() => {
+          fetchItems(prev); 
+          currentPage.current = Number(prev.match(/page=(\d+)/)?.pop()) || 
+            1;
+        }}
+        className={`
+          ${currentPage.current === 1 ? 
+            'bg-unfocused text-foreground pointer-events-none' : 
+            'bg-accent text-background'
+          }
           px-1 
           py-2
           cursor-pointer
           transition-all
           duration-300
           hover:bg-confirm-hover
-      ">
+      `}>
         <FaChevronLeft size={20} />
       </span>
 
@@ -51,25 +63,34 @@ export default function Pager({ count, next, prev, fetchItems, route }) {
           <a 
             key={index} 
             onClick={() => {
-              fetchItems(`${route}?page=${page}`)
+              fetchItems(`${route}?page=${page}`);
+              currentPage.current = page;
             }}
-            className="
+            className={`
               leading-none 
-              px-2 
-              py-2 
+              p-2
+              ${currentPage.current === page && 'bg-active'}
               cursor-pointer 
               transition-all
               duration-300
               hover:bg-active
-          ">
+          `}>
             {page}
           </a>
         ))}
       </nav>
 
       <span 
-        onClick={() => fetchItems(next)}
-        className="
+        onClick={() => {
+          fetchItems(next);
+          currentPage.current = Number(next.match(/page=(\d+)/).pop()) || 
+            number;
+        }}
+        className={`
+          ${currentPage.current === number ? 
+            'bg-unfocused text-foreground pointer-events-none' : 
+            'bg-accent text-background'
+          }
           bg-accent 
           text-background 
           px-1 
@@ -78,21 +99,26 @@ export default function Pager({ count, next, prev, fetchItems, route }) {
           transition-all
           duration-300
           hover:bg-confirm-hover
-      ">
+      `}>
         <FaChevronRight size={20} />
       </span>
       <span 
-        onClick={() => fetchItems(`${route}?page=last`)}
-        className="
-          bg-accent 
-          text-background 
+        onClick={() => {
+          fetchItems(`${route}?page=last`);
+          currentPage.current = number
+        }}
+        className={`
+          ${currentPage.current === number ? 
+            'bg-unfocused text-foreground pointer-events-none' : 
+            'bg-accent text-background'
+          }
           py-px
           leading-none
           cursor-pointer
           transition-all
           duration-300
           hover:bg-confirm-hover
-      ">
+      `}>
         <FiChevronsRight size={34} />
       </span>
     </div>
